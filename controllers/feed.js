df6d9f94f184b42cd5dab11009e7eb778b982a11
Dinika <dinikasaxenas@gmail.com>
2020-03-29 23:46:37 +0200

    Add route to delete posts
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- controllers/feed.js
+++ controllers/feed.js
@@ -127,6 +127,30 @@ exports.updatePost = (req, res, next) => {
     })
 }
 
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId
+  Post.findById(postId)
+    .then(post => {
+      // CHeck post belongs to user
+      clearImage(post.imageUrl)
+      if (!post) {
+        const error = new Error('Could not find the post')
+        error.statusCode = 404
+        throw error
+      }
+      return Post.findByIdAndRemove(postId)
+    })
+    .then(result => {
+      res.status(200).json({ message: 'Post deleted' })
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
+    })
+}
+
 const clearImage = (filePath) => {
   filePath = path.join(__dirname, '..', filePath)
   fs.unlink(filePath, err => {
