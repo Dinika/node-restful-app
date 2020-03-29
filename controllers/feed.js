b118f4da64623b67f087aaaed75128c651221e7b
Dinika <dinikasaxenas@gmail.com>
2020-03-29 23:39:41 +0200

    Add route to update posts
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator')
 const Post = require('../models/post')
+const fs = require('fs')
+const path = require('path')
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -24,8 +26,6 @@ exports.createPost = (req, res, next) => {
     error.statusCode = 422
     throw error
   }
-  console.log("IMAGE", req.image)
-  console.log("FILE", req.file)
 
   if (!req.file) {
     const error = new Error('No image provided')
@@ -81,4 +81,55 @@ exports.getPost = (req, res, next) => {
       }
       next(err)
     })
+}
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect')
+    error.statusCode = 422
+    throw error
+  }
+  const { title, content } = req.body
+  const imageUrl = req.file ? req.file.path : req.body.image
+  if (!imageUrl) {
+    const error = new Error('No image uploaded')
+    error.statusCode = 422
+    throw error
+  }
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find the post')
+        error.statusCode = 404
+        throw error
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl)
+      }
+      post.title = title
+      post.imageUrl = imageUrl
+      post.content = content
+      return post.save()
+    })
+    .then(result => {
+      res.status(201).json({
+        message: 'Post successfully updated',
+        post: result
+      })
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
+    })
+}
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, '..', filePath)
+  fs.unlink(filePath, err => {
+    console.log(err)
+  })
 }
