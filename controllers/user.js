cce653d3ed01d2c4b25d78733011b408b8e2c077
Dinika <dinikasaxenas@gmail.com>
2020-03-31 00:28:50 +0200

    Handle incoming status get and update requests
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user')
+const { validationResult } = require('express-validator')
 
 exports.getStatus = (req, res, next) => {
   const userId = req.userId
@@ -18,6 +19,39 @@ exports.getStatus = (req, res, next) => {
       res.status(200).json({
         message: 'Status successfully retrieved',
         status: user.status
+      })
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
+    })
+}
+
+exports.updateStatus = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect')
+    error.statusCode = 422
+    throw error
+  }
+
+  const userId = req.userId
+  if (!userId) {
+    const authError = new Error('Not authenticated')
+    authError.statusCode = 401
+    throw authError
+  }
+
+  User.findById(userId)
+    .then(user => {
+      user.status = req.body.status
+      return user.save()
+    })
+    .then(result => {
+      res.status(201).json({
+        message: 'Status successfully updated'
       })
     })
     .catch(err => {
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const statusController = require('../controllers/user')
+const userController = require('../controllers/user')
 const isAuth = require('../middleware/isAuth')
+const { body } = require('express-validator')
 
 const router = express.Router()
 
-router.get('/status', isAuth, statusController.getStatus)
+router.get('/status', isAuth, userController.getStatus)
+router.post('/status', isAuth, [
+  body('status').trim().isLength({ min: 2 }).withMessage('Status should be atleast 2 characters long')
+], userController.updateStatus)
 
 module.exports = router
