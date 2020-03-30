ba195d9f55ab85891cbdd10a21cdee320fad0fe6
Dinika <dinikasaxenas@gmail.com>
2020-03-30 19:26:35 +0200

    Associate users to posts they created
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,6 +2,7 @@ const { validationResult } = require('express-validator')
 const Post = require('../models/post')
 const fs = require('fs')
 const path = require('path')
+const User = require('../models/user')
 
 exports.getPosts = (req, res, next) => {
   const page = req.query.page || 1
@@ -44,20 +45,31 @@ exports.createPost = (req, res, next) => {
   }
   const imageUrl = req.file.path
   const { title, content } = req.body
+  let creator
   const post = new Post({
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: {
-      name: 'Dinika'
-    }
+    creator: req.userId
   })
   post
     .save()
     .then(result => {
+      return User.findById(req.userId)
+    })
+    .then(user => {
+      creator = user
+      user.posts.push(post)
+      return user.save()
+    })
+    .then(result => {
       res.status(201).json({
         message: 'Post created successfully',
-        post: result
+        post: post,
+        creator: {
+          _id: creator._id,
+          name: creator.name
+        }
       })
     })
     .catch(err => {
@@ -95,6 +107,7 @@ exports.getPost = (req, res, next) => {
 
 exports.updatePost = (req, res, next) => {
   const postId = req.params.postId
+  console.log("HERE", postId)
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed, entered data is incorrect')
