eedd54b0436a5117de51d4e4b4a8e87fa749de70
Dinika <dinikasaxenas@gmail.com>
2020-04-03 12:58:52 +0200

    Support pagination
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- graphql/schema.js
+++ graphql/schema.js
@@ -44,7 +44,7 @@ module.exports = buildSchema(`
 
   type RootQuery {
     login(email: String!, password: String!): authData!
-    getPosts: PostData
+    getPosts(page: Int): PostData
   }
 
   type RootMutation {
