74c1a50321320945c7e2f7161e146f97d7fe5eb8
Dinika <dinikasaxenas@gmail.com>
2020-04-03 22:59:59 +0200

    ADd resolver for updateStatus mutation
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -230,5 +230,44 @@ module.exports = {
     catch (err) {
       return false
     }
-  }
+  },
+
+  user: async function (arg, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated')
+      error.code = 401
+      throw error
+    }
+    const user = await User.findById(req.userId)
+    if (!user) {
+      const error = new Error("No user found")
+      error.code = 404
+      throw error
+    }
+    return {
+      ...user._doc,
+      _id: user._id.toString(),
+    }
+  },
+
+  updateStatus: async function ({ status }, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated')
+      error.code = 401
+      throw error
+    }
+    const user = await User.findById(req.userId)
+    if (!user) {
+      const error = new Error("No user found")
+      error.code = 404
+      throw error
+    }
+    user.status = status
+    await user.save()
+    return {
+      ...user._doc,
+      _id: user._id.toString(),
+    }
+  },
+
 }
